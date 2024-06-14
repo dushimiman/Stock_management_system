104a16df422d5a1522541f9921994d9f6c2e94b8
dushimiman <dushimediane12@gmail.com>
2024-06-14 14:06:38 +0200

    fist commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+</div>
+<div class="container-fluid footer">
+    
+<script src="./assets/jquery/jquery-3.2.1.js"></script>
+<script src="./assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="./assets/plugins/datatables/dataTables.bootstrap.js"></script>
+<script src="./assets/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="./assets/custom/js/custom.js"></script>
+</body>
+</html>
